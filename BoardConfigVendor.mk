de21e3cce2c1de2f91e8d4af7ed617baae79ea34
chandu078 <chandudyavanapelli03@gmail.com>
2026-04-26 08:29:24 +0000

    fairlady: Initial import proprietary files from OOS 11.A.19
    Change-Id: I49d4ce5920e9b193d006efccabf9cf3c94d7cb09

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,45 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    hyp_ac_config \
+    imagefv \
+    keymaster \
+    modem \
+    multiimgoem \
+    multiimgqti \
+    oplus_sec \
+    pdp \
+    pdp_cdb \
+    qupfw \
+    secretkeeper \
+    shrm \
+    soccp \
+    soccp_dcd \
+    soccp_debug \
+    splash \
+    spuservice \
+    tme_config \
+    tme_fw \
+    tme_seq_patch \
+    tz \
+    tz_ac_config \
+    tz_qti_config \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_ac_config \
+    xbl_config \
+    xbl_ramdump
